8837cbae4e642c4b24891c71f45841a9001598cd
Muhammad Isa <msawjk@gmail.com>
2022-01-06 14:46:57 +0700

    seperate middleware

--- middlewares/middlewares.go
+++ middlewares/middlewares.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/afex/hystrix-go/hystrix"
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
@@ -49,14 +50,17 @@ func CircuitBreakerMiddleware(command string) endpoint.Middleware {
 				return nil, logicErr
 			}
 			if err != nil {
+				errMsg := fmt.Sprintf(
+					"service %s is busy or unavailable, please try again later => detail error : %s",
+					command,
+					err.Error(),
+				)
 				return nil, status.Error(
 					codes.Unavailable,
-					errors.New("service is busy or unavailable, please try again later").Error(),
+					errors.New(errMsg).Error(),
 				)
 			}
 			return resp, nil
 		}
 	}
 }
-
-
