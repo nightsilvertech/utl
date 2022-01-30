2d332fa142f8d67e78ef4234f74feb7bbf2d9663
Muhammad Isa <msawjk@gmail.com>
2022-01-30 18:55:25 +0700

    fixing util

--- fcmsender/fcmsender.go
+++ fcmsender/fcmsender.go
@@ -30,42 +30,46 @@ type Notification struct {
 	Notif Notif  `json:"notification"`
 }
 
-func Send(appKey string, notification Notification) (res string, err error) {
-	headers := []string{
-		fmt.Sprintf("Authorizationa%skey=%s", headerSeparator, appKey),
-	}
-	jsonData, err := json.Marshal(notification)
-	if err != nil {
-		return res, err
-	}
-	req, err := http.NewRequest(http.MethodPost, googleFcmApiBaseURL, bytes.NewBuffer(jsonData))
-	if err != nil {
-		return res, err
-	}
-	for _, header := range headers {
-		keyValue := strings.Split(header, headerSeparator)
-		if len(keyValue) == headerLength {
-			req.Header.Set(keyValue[keyIndex], keyValue[valueIndex])
+func Send(appKey string, notification Notification, pretend bool) (res string, err error) {
+	if pretend {
+		return "SENT", nil
+	} else {
+		headers := []string{
+			fmt.Sprintf("Authorizationa%sKey=%s", headerSeparator, appKey),
 		}
-	}
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return res, err
-	}
-	defer func(Body io.ReadCloser) {
-		_ = Body.Close()
-	}(resp.Body)
+		jsonData, err := json.Marshal(notification)
+		if err != nil {
+			return res, err
+		}
+		req, err := http.NewRequest(http.MethodPost, googleFcmApiBaseURL, bytes.NewBuffer(jsonData))
+		if err != nil {
+			return res, err
+		}
+		for _, header := range headers {
+			keyValue := strings.Split(header, headerSeparator)
+			if len(keyValue) == headerLength {
+				req.Header.Set(keyValue[keyIndex], keyValue[valueIndex])
+			}
+		}
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			return res, err
+		}
+		defer func(Body io.ReadCloser) {
+			_ = Body.Close()
+		}(resp.Body)
 
-	if resp.StatusCode > 201 {
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-		msg := fmt.Sprintf("http status code %d error response %s", resp.StatusCode, string(bodyBytes))
-		return res, errors.New(msg)
-	}
+		if resp.StatusCode > 201 {
+			bodyBytes, _ := ioutil.ReadAll(resp.Body)
+			msg := fmt.Sprintf("http status code %d error response %s", resp.StatusCode, string(bodyBytes))
+			return res, errors.New(msg)
+		}
 
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return res, err
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return res, err
+		}
+		return string(bodyBytes), nil
 	}
-	return string(bodyBytes), nil
 }
